d33317f14757d34b2ec424ddf8bfbd2280b0f197
qiongtubao <810776886@qq.com>
2013-09-16 10:06:36 +0800

    http查询功能

--- game-server/app/lib/http/httpClient.js
+++ game-server/app/lib/http/httpClient.js
@@ -14,8 +14,54 @@ var httpClient = module.exports;
  * @param params
  * @param post_body
  */
-httpClient.request = function(type, header, params, post_body) {
+var url = require('url');
+httpClient.request = function(type, header, params, post_body,callback) {
+    if(typeof params == 'string' && params.indexOf('http')==-1) {
+        params = 'http://'+params;
+    }
+    var content = null;
+    header = header || {};
+    if(post_body){
+        content = require('querystring').stringify(post_body);
+        header['Content-length'] = content.length;
+    }
 
+    var parse_url = url.parse(params,true);
+    var htype = parse_url.protocol.slice(0,-1) || 'http';
+    console.log(parse_url);
+    var options = {
+        hostname:parse_url.hostname || 'localhost'
+        ,port:parse_url.port
+        ,path:parse_url.path
+        ,headers:header
+        ,method: type.toUpperCase()
+    }
+    var req = require(htype).get(options,function(res) {
+        var _data = '';
+        res.on('data',function(chunk){
+            _data +=  chunk;
+        })
+        res.on('end',function() {
+            callback(null,_data);
+        });
+    });
+    req.on('error',function(err){
+       callback(err.message,null);
+    });
+    req.setTimeout(5000,function() {
+        callback('error:'+params+' timeout!',null);
+    });
+    req.write(content);
+    req.end();
+}
+/**
+ *
+ * @param header
+ * @param params
+ * @param post_body
+ */
+httpClient.get = function(header, params, post_body,callback) {
+   this.request('get',header,params,post,post_body,callback);
 }
 
 /**
@@ -24,18 +70,6 @@ httpClient.request = function(type, header, params, post_body) {
  * @param params
  * @param post_body
  */
-httpClient.get = function(header, params, post_body) {
-    var type = "get";
-    httpClient.request(type, header, params, post_body);
-}
-
-/**
- *
- * @param header
- * @param params
- * @param post_body
- */
-httpClient.post = function(header, params, post_body) {
-    var type = "post";
-    httpClient.request(type, header, params, post_body);
+httpClient.post = function(header, params, post_body,callback) {
+    this.request('post',header,params,post,post_body,callback);
 }
