5add80227ecf6bcff9bedc671a881b77292b8aa9
zhangchunsheng <zhangchunsheng423@gmail.com>
2013-09-19 16:41:57 +0800

    1、新功能：增加技能功能

---
+++ game-server/app/domain/entity/fightskill.js
@@ -0,0 +1,70 @@
+/**
+ * Copyright(c)2013,Wozlla,www.wozlla.com
+ * Version: 1.0
+ * Author: Peter Zhang
+ * Date: 2013-06-28
+ * Description: fightskill
+ */
+
+var util = require('util');
+var dataApi = require('../../util/dataApi');
+var formula = require('../../consts/formula');
+var consts = require('../../consts/consts');
+var buff = require('./buff');
+var logger = require('pomelo-logger').getLogger(__filename);
+
+/**
+ * 计算技能攻击伤害
+ *
+ * @param attacker
+ * @param target
+ * @param skill
+ */
+var attack = function(formation, attacker, targets, skill) {
+
+};
+
+var calculateAttack = function() {
+
+}
+
+var calculateDefense = function() {
+
+}
+
+/**
+ *
+ * @param attacker
+ * @param target
+ * @param buff
+ * @returns {{result: *}}
+ */
+var addBuff = function(attacker, target, buff) {
+    if (buff.target === 'attacker' && !attacker.died) {
+        buff.use(attacker);
+    } else if (buff.target === 'target' && !target.died) {
+        buff.use(target);
+    }
+    return {
+        result: consts.AttackResult.SUCCESS
+    };
+};
+
+var removeBuff = function(attacker, target, buff) {
+
+};
+
+/**
+ *
+ * @param opts
+ * @constructor
+ */
+var FightSkill = function(opts) {
+    this.skillId = opts.skillId;
+    this.level = opts.level;
+    this.playerId = opts.playerId;
+    this.skillData = dataApi.fightskill.findById(this.skillId);
+    this.name = this.skillData.skillName;
+};
+
+module.exports.FightSkill = FightSkill;
