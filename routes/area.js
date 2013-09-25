8bfdc1ec46f5fcc824ea3a946ec7b714837e72d4
zhangchunsheng <zhangchunsheng423@gmail.com>
2013-09-25 22:17:16 +0800

    3、新功能：新增初始化技能功能 4、新功能：调整创建角色功能

---
+++ routes/area.js
@@ -0,0 +1,34 @@
+/**
+ * Copyright(c)2013,Wozlla,www.wozlla.com
+ * Version: 1.0
+ * Author: Peter Zhang
+ * Date: 2013-09-24
+ * Description: area
+ */
+var areaService = require('../app/services/areaService');
+var Code = require('../shared/code');
+var utils = require('../app/utils/utils');
+var consts = require('../app/consts/consts');
+var EntityType = require('../app/consts/consts').EntityType;
+var dataApi = require('../app/utils/dataApi');
+var area = require('../app/domain/area/area');
+var world = require('../app/domain/world');
+var async = require('async');
+
+exports.index = function(req, res) {
+    res.send("index");
+}
+
+exports.getAreaInfo = function(req, res) {
+    var msg = req.query;
+    var session = req.session;
+
+    var sceneId = msg.sceneId;
+    area.getAreaPlayers(sceneId, function(err, results) {
+        var data = {
+            code: consts.MESSAGE.RES,
+            entities: results
+        };
+        utils.send(msg, res, data);
+    });
+}
