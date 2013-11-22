683450c33a4d3598bfd663bc627703028bc67a77
zhangchunsheng <zhangchunsheng423@gmail.com>
2013-11-23 01:02:04 +0800

    1、新功能：更新技能数值

---
+++ scripts/buff_scriptV2.js
@@ -0,0 +1,12 @@
+/**
+ * Copyright(c)2013,Wozlla,www.wozlla.com
+ * Version: 1.0
+ * Author: Peter Zhang
+ * Date: 2013-11-23
+ * Description: buff_scriptV2
+ */
+var buff_script = {
+
+}
+
+module.exports = buff_script;
